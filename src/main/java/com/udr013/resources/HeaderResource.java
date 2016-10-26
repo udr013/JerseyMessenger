6f2a999a9b8457ba5ecfb27ce31ee5a3c2df6fc8
¨udr013¨ <udr013@gmail.com>
2016-10-27 00:17:03 +0200

    Using @Context with httpHeaders and uriInfo

--- src/main/java/com/udr013/resources/HeaderResource.java
+++ src/main/java/com/udr013/resources/HeaderResource.java
@@ -1,7 +1,12 @@
 package com.udr013.resources;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Smal illustration on how to read headers
@@ -17,4 +22,22 @@ public class HeaderResource {
 		// there is als a @FormParam but  not  widely used
 		return "HeaderParam value of blabla: " + header +"/n This is myCookie: " + cookie;
 	}
+
+	@GET
+	@Path("/context")
+	public String getParamUsingContext(@Context UriInfo uriInfo, @Context HttpHeaders httpHeaders){
+
+	String uriInfoString = uriInfo.getAbsolutePath().toString();
+	String kookie = httpHeaders.getCookies().toString();
+	Map httpHeaderMap = httpHeaders.getRequestHeaders();
+
+	List bla = httpHeaders.getAcceptableMediaTypes();
+
+		return " This is the uriInfoString " +
+				"AbsolutePath: " + uriInfoString + "###  httpHeaderMap: ### " + httpHeaderMap.entrySet() + " " +
+				" ### httpHeaders.getAcceptableMediaTypes: ### "+ bla.get(0).toString() +", " +bla.get(1).toString()+
+				" ### kookie ###" + kookie;
+
+	}
+
 }
